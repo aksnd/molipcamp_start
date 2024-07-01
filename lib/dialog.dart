4a0e2dd7931bd9627812aa498260ca4e9519f090
pjh04 <pjh0490@naver.com>
2024-07-01 19:52:34 +0900

    master와 merge

--- lib/dialog.dart
+++ lib/dialog.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import './contact.dart';
 import 'dart:io';
@@ -107,147 +105,155 @@ Future<void> editProfile(BuildContext context, SimpleContact contact, onUpdate) 
   final TextEditingController _controller3 = TextEditingController(text: editedContact.birthday);
   final TextEditingController _controller4 = TextEditingController(text: editedContact.mbti);
 
-  Future getImage(ImageSource imageSource) async {
-    //pickedFile에 ImagePicker로 가져온 이미지가 담긴다.
-    final ImagePicker picker = ImagePicker();
-    final XFile? pickedFile = await picker.pickImage(source: imageSource);
-    if (pickedFile != null) {
-      editedContact.image = pickedFile.path;
-    }
-  }
+
   return showDialog<void>(
     context: context,
     barrierDismissible: false, // user must tap button to dismiss
-    builder: (BuildContext context) {
-      return AlertDialog(
-        //title: Text('Input Dialog'),
-        contentPadding: EdgeInsets.all(10),
-        content: SingleChildScrollView(
-          child: Column(
-            children: [
-              // _getImageProvider(contact.image),
-              SizedBox(height: 20,),
-              Container(
-                width: 200, // 원하는 크기로 설정하세요
-                height: 200, // 원하는 크기로 설정하세요
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(8.0),
-                  child: _getImageProvider(contact.image),
-                ),
+    builder: (context){
+      return StatefulBuilder(
+        builder: (context, setState){
+          Future getImage(ImageSource imageSource) async {
+            //pickedFile에 ImagePicker로 가져온 이미지가 담긴다.
+            final ImagePicker picker = ImagePicker();
+            final XFile? pickedFile = await picker.pickImage(source: imageSource);
+            if (pickedFile != null) {
+              setState((){
+                editedContact.image = pickedFile.path;
+              });
+            }
+          }
+          return AlertDialog(
+            //title: Text('Input Dialog'),
+            contentPadding: EdgeInsets.all(10),
+            content: SingleChildScrollView(
+              child: Column(
+                children: [
+                  // _getImageProvider(contact.image),
+                  SizedBox(height: 20,),
+                  Container(
+                    width: 200, // 원하는 크기로 설정하세요
+                    height: 200, // 원하는 크기로 설정하세요
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
+                      child: _getImageProvider(editedContact.image),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: <Widget>[
+                            Text(
+                              "이름 ",
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.grey[700],
+                              ),
+                            ),
+                            const SizedBox(width: 8), // 간격 조정을 위한 SizedBox
+                            Expanded(
+                              child: TextField(
+                                controller: _controller1,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 8),
+                        Row(
+                          children: <Widget>[
+                            Text(
+                              "전화번호 ",
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.grey[700],
+                              ),
+                            ),
+                            const SizedBox(width: 8), // 간격 조정을 위한 SizedBox
+                            Expanded(
+                              child: TextField(
+                                controller: _controller2,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 8),
+                        Row(
+                          children: <Widget>[
+                            Text(
+                              "생일 ",
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.grey[700],
+                              ),
+                            ),
+                            const SizedBox(width: 8), // 간격 조정을 위한 SizedBox
+                            Expanded(
+                              child: TextField(
+                                controller: _controller3,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 8),
+                        Row(
+                          children: <Widget>[
+                            Text(
+                              "MBTI ",
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.grey[700],
+                              ),
+                            ),
+                            const SizedBox(width: 8), // 간격 조정을 위한 SizedBox
+                            Expanded(
+                              child: TextField(
+                                controller: _controller4,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: <Widget>[
-                        Text(
-                          "이름 ",
-                            style: TextStyle(
-                              fontSize: 18,
-                              color: Colors.grey[700],
-                            ),
-                        ),
-                        const SizedBox(width: 8), // 간격 조정을 위한 SizedBox
-                        Expanded(
-                          child: TextField(
-                            controller: _controller1,
-                            ),
-                          ),
-                      ],
-                    ),
-                    SizedBox(height: 8),
-                    Row(
-                      children: <Widget>[
-                        Text(
-                          "전화번호 ",
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.grey[700],
-                          ),
-                        ),
-                        const SizedBox(width: 8), // 간격 조정을 위한 SizedBox
-                        Expanded(
-                          child: TextField(
-                            controller: _controller2,
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 8),
-                    Row(
-                      children: <Widget>[
-                        Text(
-                          "생일 ",
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.grey[700],
-                          ),
-                        ),
-                        const SizedBox(width: 8), // 간격 조정을 위한 SizedBox
-                        Expanded(
-                          child: TextField(
-                            controller: _controller3,
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 8),
-                    Row(
-                      children: <Widget>[
-                        Text(
-                          "MBTI ",
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.grey[700],
-                          ),
-                        ),
-                        const SizedBox(width: 8), // 간격 조정을 위한 SizedBox
-                        Expanded(
-                          child: TextField(
-                            controller: _controller4,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: Text('image'),
+                onPressed: () {
+                  getImage(ImageSource.gallery);
+                },
+              ),
+              TextButton(
+                child: Text('Close'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              TextButton(
+                child: Text('Save'),
+                onPressed: () {
+                  editedContact.name = _controller1.text;
+                  editedContact.phone = _controller2.text;
+                  editedContact.birthday = _controller3.text;
+                  onUpdate(contact.index, editedContact);
+                  editedContact.mbti = _controller4.text;
+                  print(_controller1.text);
+                  print(_controller2.text);
+                  print(_controller3.text);
+                  print(_controller4.text);
+                  Navigator.of(context).pop();
+                },
               ),
             ],
-          ),
-        ),
-        actions: <Widget>[
-          TextButton(
-            child: Text('image'),
-            onPressed: () {
-              getImage(ImageSource.gallery);
-            },
-          ),
-          TextButton(
-            child: Text('Close'),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-          TextButton(
-            child: Text('Save'),
-            onPressed: () {
-              editedContact.name = _controller1.text;
-              editedContact.phone = _controller2.text;
-              editedContact.birthday = _controller3.text;
-              onUpdate(contact.index, editedContact);
-              editedContact.mbti = _controller4.text;
-              print(_controller1.text);
-              print(_controller2.text);
-              print(_controller3.text);
-              print(_controller4.text);
-              Navigator.of(context).pop();
-            },
-          ),
-        ],
+          );
+        },
+
       );
-    },
+    }
   );
 
 
