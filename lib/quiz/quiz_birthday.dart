fb35ca3d61592b833ab0ef31727331c080e6efdb
kimyerak <yerak213@gmail.com>
2024-07-03 17:52:27 +0900

    real-real-final

--- lib/quiz/quiz_birthday.dart
+++ lib/quiz/quiz_birthday.dart
@@ -10,7 +10,7 @@ import 'quiz_phonenumber.dart';
 import 'package:kaist_week1/main.dart';
 import 'package:kaist_week1/quiz/utils/calculator.dart';
 import 'package:kaist_week1/quiz/utils/progress_bar.dart';
-import 'package:kaist_week1/quiz/utils/checkAnswer.dart';
+// import 'package:kaist_week1/quiz/utils/checkAnswer.dart';
 
 //생일 퀴즈 - 탭3
 class BirthdayQuizPage extends StatefulWidget {
@@ -61,6 +61,38 @@ class _BirthdayQuizPageState extends State<BirthdayQuizPage> {
     }
 
     options.shuffle();
+  }
+  void _checkAnswer(String selectedOption) {
+    bool isCorrect = selectedOption == correctAnswer;
+    if (isCorrect) {
+      answercount_birth++;
+    }
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(isCorrect ? '정답!' : '오답'),
+          content: Text(
+              isCorrect ? '축하합니다! 정답입니다.' : '아쉽네요. 정답은 ${correctAnswer}입니다.'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('닫기'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                _nextQuestion();
+              },
+              child: Text('다음문제'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
 
@@ -342,11 +374,7 @@ class _BirthdayQuizPageState extends State<BirthdayQuizPage> {
                               .map((option) => Padding(
                             padding: const EdgeInsets.all(5.0),
                             child: ElevatedButton(
-                              onPressed: () => checkAnswer(context: context,
-                                selectedOption: option,
-                                correctAnswer: correctAnswer,
-                                answerCount: answercount_birth,
-                                nextQuestion: _nextQuestion,),
+                              onPressed: () => _checkAnswer(option),
                               style: ElevatedButton.styleFrom(
                                 backgroundColor: Color(0xFF8ECAE6),
                                 shape: RoundedRectangleBorder(
