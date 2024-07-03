290ccddbfbe1f1bceb078aeab484d6566715c11d
kimyerak <yerak213@gmail.com>
2024-07-03 17:24:24 +0900

    finallygit add .

--- lib/mode_select.dart
+++ lib/mode_select.dart
@@ -11,7 +11,8 @@ class ModeSelectionPage extends StatefulWidget {
   _ModeSelectionPageState createState() => _ModeSelectionPageState();
 }
 
-class _ModeSelectionPageState extends State<ModeSelectionPage> with SingleTickerProviderStateMixin{
+class _ModeSelectionPageState extends State<ModeSelectionPage>
+    with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Animation<double> _animation;
 
@@ -61,34 +62,36 @@ class _ModeSelectionPageState extends State<ModeSelectionPage> with SingleTicker
               ),
               SizedBox(height: 20),
               AnimatedBuilder(
-          animation: _animation,
-          builder: (context, child) {
-            return Transform.scale(
-              scale: _animation.value,
-                child: Container(
-                          padding: EdgeInsets.all(8.0),
-                          decoration: BoxDecoration(
-                            color: Color(0xFF023047),
-                            borderRadius: BorderRadius.circular(20),),
-                  child: Container(
-                      width: 180,
-
-                      alignment: Alignment.centerRight,
-                      child: GroupDropdown(
-                                              groups: groups,
-                                              selectedGroup: dropDownGroup[2],
-                                              onGroupChanged: (String newGroup) {
-                      dropDownGroup[2] = newGroup;
-                      Provider.of<ContactsProvider>(context, listen: false)
-                          .updateNowGroup(dropDownGroup, 2);
-                                              },
-                                              widgetFrom: 2,
-                                            )
-                  ),
-                ),
-              );}
-            ),
-              SizedBox(height: 30,),
+                  animation: _animation,
+                  builder: (context, child) {
+                    return Transform.scale(
+                      scale: _animation.value,
+                      child: Container(
+                        padding: EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                          color: Color(0xFF023047),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Container(
+                            width: 180,
+                            alignment: Alignment.centerRight,
+                            child: GroupDropdown(
+                              groups: groups,
+                              selectedGroup: dropDownGroup[2],
+                              onGroupChanged: (String newGroup) {
+                                dropDownGroup[2] = newGroup;
+                                Provider.of<ContactsProvider>(context,
+                                        listen: false)
+                                    .updateNowGroup(dropDownGroup, 2);
+                              },
+                              widgetFrom: 2,
+                            )),
+                      ),
+                    );
+                  }),
+              SizedBox(
+                height: 30,
+              ),
               ElevatedButton(
                 onPressed: () {
                   Navigator.push(
@@ -99,48 +102,14 @@ class _ModeSelectionPageState extends State<ModeSelectionPage> with SingleTicker
                   );
                 },
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFF8ECAE6),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  width: 180,
-                  color: Color(0xFF3AB349),
-                  alignment: Alignment.centerRight,
-                  child:GroupDropdown(
-                    groups: groups,
-                    selectedGroup: dropDownGroup[2],
-                    onGroupChanged:(String newGroup){
-                      dropDownGroup[2]= newGroup;
-                      Provider.of<ContactsProvider>(context, listen: false).updateNowGroup(dropDownGroup, 2);
-                    },
-                    widgetFrom: 2,
-                  )
+                    backgroundColor: Color(0xFF8ECAE6),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    )),
+                child: Text(
+                  '퀴즈 시작!',
+                  style: TextStyle(color: Color(0xFF023047)),
                 ),
-                SizedBox(height: 20,),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => free_page(),
-                      ),
-                    );
-                  },
-              style: ElevatedButton.styleFrom(
-          backgroundColor: Color(0xFF8ECAE6),
-          shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20),
-                  ),
-                ),
-                child: Text('퀴즈 시작!', style: TextStyle(color: Color(0xFF023047), fontWeight: FontWeight.bold),),
-
-
-              ),
-                child: Text('퀴즈 시작!', style: TextStyle(color: Color(0xFF023047)),),
               ),
             ],
           ),
